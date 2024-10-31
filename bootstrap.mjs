c6084ba8692c61a784ecf41dafaa2334bf74d18c
Cheng <git@zcbenz.com>
2024-10-31 11:18:56 +0900

    Accelerate bootstrap

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -5,4 +5,11 @@ import {python} from './common.mjs';
 await $`git submodule sync --recursive`
 await $`git submodule update --init --recursive`
 
+// Accelerate install_requirements.py which builds native deps.
+process.env.CMAKE_BUILD_PARALLEL_LEVEL = os.cpus().length
+try {
+  // Use ccache when available.
+  process.env.CMAKE_ARGS = `-DCMAKE_CXX_COMPILER_LAUNCHER=${await which('ccache')}`
+} catch {}
+
 await $({cwd: 'executorch'})`${python} install_requirements.py`
